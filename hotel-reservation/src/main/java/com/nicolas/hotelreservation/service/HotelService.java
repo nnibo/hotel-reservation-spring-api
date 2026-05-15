2907e9fad29a7015d2bf91a934364e5a21e2fa55
nnibo <nbaradel4@gmail.com>
2026-05-15 15:42:01 -0300

    feat - creating update hotel and delete hotel methods in hotel service

--- hotel-reservation/src/main/java/com/nicolas/hotelreservation/service/HotelService.java
+++ hotel-reservation/src/main/java/com/nicolas/hotelreservation/service/HotelService.java
@@ -3,6 +3,8 @@ package com.nicolas.hotelreservation.service;
 import com.nicolas.hotelreservation.dto.request.HotelRequestDTO;
 import com.nicolas.hotelreservation.dto.response.HotelResponseDTO;
 import com.nicolas.hotelreservation.entity.HotelEntity;
+import com.nicolas.hotelreservation.exception.BadRequestException;
+import com.nicolas.hotelreservation.exception.NotFoundException;
 import com.nicolas.hotelreservation.repository.IHotelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,11 +17,11 @@ import java.util.stream.Collectors;
 public class HotelService {
     private final IHotelRepository hotelRepository;
 
-    public void createHotel(HotelRequestDTO hotelDTO) throws Exception{
+    public void createHotel(HotelRequestDTO hotelDTO) throws BadRequestException {
         HotelEntity hotel = hotelRepository.findByName(hotelDTO.name()).orElse(null);
 
         if(hotel != null){
-            throw new Exception("Hotel já cadastrado no sistema");
+            throw new BadRequestException("Hotel já cadastrado no sistema");
         }
 
         hotelRepository.save(HotelEntity.builder()
@@ -44,9 +46,34 @@ public class HotelService {
                                         .toList();
     }
 
-    public HotelResponseDTO getHotelById(Long id) throws Exception{
-        HotelEntity hotel = hotelRepository.findById(id).orElseThrow(() -> new Exception("Hotel não encontrado"));
+    public HotelResponseDTO getHotelById(Long id) throws NotFoundException{
+        HotelEntity hotel = hotelRepository.findById(id).orElseThrow(() -> new NotFoundException("Hotel não encontrado"));
 
         return new HotelResponseDTO(hotel.getId(), hotel.getName(), hotel.getDescription(), hotel.getCity(), hotel.getAddress());
+    }
+
+    public HotelResponseDTO updateHotelById(Long id, HotelRequestDTO hotelRequestDTO) throws NotFoundException {
+        HotelEntity hotel = hotelRepository.findById(id).orElseThrow(() -> new NotFoundException("Hotel não encontrado"));
+
+        hotel.setName(hotelRequestDTO.name());
+        hotel.setDescription(hotelRequestDTO.description());
+        hotel.setCity(hotelRequestDTO.city());
+        hotel.setAddress(hotelRequestDTO.address());
+
+        HotelEntity updatedHotel = hotelRepository.save(hotel);
+
+        return new HotelResponseDTO(
+                updatedHotel.getId(),
+                updatedHotel.getName(),
+                updatedHotel.getDescription(),
+                updatedHotel.getCity(),
+                updatedHotel.getAddress()
+        );
+    }
+
+    public void deleteHotel(Long id) throws NotFoundException{
+        HotelEntity hotel = hotelRepository.findById(id).orElseThrow(() -> new NotFoundException("Hotel não encontrado"));
+
+        hotelRepository.delete(hotel);
     }
 }
