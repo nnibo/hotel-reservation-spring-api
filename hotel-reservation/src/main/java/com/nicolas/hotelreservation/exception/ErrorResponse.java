2fc10956c5dd7e51c23d490f3849e516dff5cca3
nnibo <nbaradel4@gmail.com>
2026-05-15 16:09:32 -0300

    feat - creating global exception handler, and not found and bad request exceptions

---
+++ hotel-reservation/src/main/java/com/nicolas/hotelreservation/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.nicolas.hotelreservation.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        int status,
+        String message,
+        LocalDateTime timestamp
+) {
+}
