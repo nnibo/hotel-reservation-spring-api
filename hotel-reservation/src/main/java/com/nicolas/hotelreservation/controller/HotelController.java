a36a8268f8b0e31d694b7f0bf0e1d57a58bec880
nnibo <nbaradel4@gmail.com>
2026-05-19 16:44:16 -0300

    feat - creating hotel controller with their basics endpoints and funcionalities

---
+++ hotel-reservation/src/main/java/com/nicolas/hotelreservation/controller/HotelController.java
@@ -0,0 +1,50 @@
+package com.nicolas.hotelreservation.controller;
+
+import com.nicolas.hotelreservation.dto.request.HotelRequestDTO;
+import com.nicolas.hotelreservation.dto.response.HotelResponseDTO;
+import com.nicolas.hotelreservation.service.HotelService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/hotels")
+public class HotelController {
+
+    private final HotelService hotelService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<HotelResponseDTO> getAllHotels(){
+        return hotelService.getAllHotels();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public HotelResponseDTO getHotelById(@PathVariable Long id) {
+        return hotelService.getHotelById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createHotel(@RequestBody @Valid HotelRequestDTO hotelDTO) {
+        hotelService.createHotel(hotelDTO);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public HotelResponseDTO updateHotel(@PathVariable Long id, @RequestBody @Valid HotelRequestDTO hotelRequestDTO) {
+        return hotelService.updateHotelById(id, hotelRequestDTO);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteHotel(@PathVariable Long id) {
+        hotelService.deleteHotel(id);
+    }
+}
